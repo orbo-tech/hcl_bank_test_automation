48a5db550d211d0f66d551b59572d64843fbb595
orbo-tech <orboton@gmail.com>
2024-02-13 16:47:30 +0000

    api tests added

--- tests/api/test_api.py
+++ tests/api/test_api.py
@@ -1,34 +1,76 @@
+from random import random
+
 import pytest
 import requests
 
+from utils.file import get_json_value
 
+expected_data_list = get_json_value("../../test_data/expected_api_data.json", "expected_data")
+
+
+@pytest.mark.api
 def test_successful_authentication(base_url, get_auth_token):
-    url = f"{base_url}/posts"
+    url = f"{base_url}/public/v2/users"
     headers = {"Authorization": f"Bearer {get_auth_token}"}
-
     response = requests.get(url, headers=headers)
-
     assert response.status_code == 200
-    assert "userId" in response.json()[0]
+    assert "email" in response.json()[0]
 
 
-def test_send_request_and_validate_response(base_url, get_auth_token):
-    url = f"{base_url}/comments/1"
-    headers = {"Authorization": f"Bearer {get_auth_token}"}
-
-    response = requests.get(url, headers=headers)
-
-    assert response.status_code == 200
-    assert "name" in response.json()
+@pytest.mark.api
+def test_post_request_and_validate_status_code(base_url, get_auth_token):
+    url = f"{base_url}/public/v2/users"
+    headers = {"Authorization": f"Bearer {get_auth_token}", }
+    response = requests.post(url, headers=headers, json=expected_data_list[0])
+    assert response.status_code == 201
 
 
+@pytest.mark.api
 def test_parse_nested_json(base_url, get_auth_token):
-    url = f"{base_url}/users/1"
+
+    """ Verify nested json response. """
+
+    url = f"{base_url}/public/v2/users"
     headers = {"Authorization": f"Bearer {get_auth_token}"}
-
     response = requests.get(url, headers=headers)
-    user_data = response.json()
 
+    actual_data_list = response.json()
     assert response.status_code == 200
-    assert "address" in user_data
-    assert "geo" in user_data["address"]
+
+    # To verify individual item form response.
+    assert expected_data_list[0]["name"] == actual_data_list[0]["name"]
+
+    # To verify all item from response using loop.
+    for actual_data in actual_data_list:
+        actual_data["name"] == expected_data_list[0]["name"]
+        actual_data["email"] == expected_data_list[0]["email"]
+        actual_data["gender"] == expected_data_list[0]["gender"]
+        actual_data["status"] == expected_data_list[0]["status"]
+
+
+@pytest.mark.api
+def test_delete_request_and_validate_status_code(base_url, get_auth_token):
+    """
+    Delete the first user from the public API v2.
+
+    Parameters:
+    - base_url (str): The base URL of the API.
+    - get_auth_token (callable): A function or variable providing the authentication token.
+
+    Raises:
+    - AssertionError: If the DELETE request does not return a status code of 204.
+    """
+
+    # Get the ID of the first user
+    url = f"{base_url}/public/v2/users/"
+    headers = {"Authorization": f"Bearer {get_auth_token}"}
+    response = requests.get(url, headers=headers)
+    user_id = response.json()[0]["id"]
+
+    # Delete the user with the obtained ID
+    url = f"{base_url}/public/v2/users/{user_id}"
+    response = requests.delete(url, headers=headers)
+
+    # Assert that the DELETE request was successful (status code 204)
+    assert response.status_code == 204, f"Failed to delete user. Status code: {response.status_code}"
+
--- utils/file.py
+++ utils/file.py
@@ -1,7 +1,7 @@
 import json
 
 
-def get_json_value(file_path, key):
+def get_json_value(file_path, key) -> list:
     """
     Read a JSON file and get the value associated with the provided key.
 
@@ -19,14 +19,3 @@ def get_json_value(file_path, key):
             return data.get(key, None)
     except (FileNotFoundError, json.JSONDecodeError):
         return None
-
-# # Example usage:
-# file_path = "path/to/your/file.json"  # Replace with the actual path
-# key_to_retrieve = "your_key"           # Replace with the actual key
-#
-# value = get_json_value(file_path, key_to_retrieve)
-# if value is not None:
-#     print(f"The value for key '{key_to_retrieve}' is: {value}")
-# else:
-#     print(f"Key '{key_to_retrieve}' not found or error reading the file.")
-#
